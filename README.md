1135ba01ef4530ed6bc72f53d9dc57b39f0316cb
Loay Omar <loayomar3010@gmail.com>
2026-05-02 19:14:53 +0200

    fichiers_préparées

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Simulateur_Assembleur
+Nom : Loay YOUNES
+
+Organisation ==> 
+1. fichier pom.xml : contenant les dépendances à installer (JUnit) et servant à organisation du projet
+2. 2 pdfs ==> diagrammes UML Classes, Cas
+3. repo sequences_boundary
+4. repo src :
+            package control
+            package boundary
+            package entities
---
+++ src/main/java/Boundary/BoundaryMenu.java
@@ -0,0 +1,12 @@
+package Boundary;
+
+import Controller.*;
+
+public class BoundaryMenu {
+    ControllerEntrerProgramme controllerEntrerProgramme;
+    ControllerAfficherProgramme controllerAfficherProgramme;
+    ControllerCompiler controllerCompiler;
+    ControllerVoirALU controllerVoirALU;
+    ControllerVoirMemoire controllerVoirMemoire;
+    ControllerVoirRegistres controllerVoirRegistres;
+}
